2204aaa761d698a6ea4a2db24f9423135d7b500e
Rino <82651074+Rino9514@users.noreply.github.com>
2025-05-09 12:12:56 +0200

    exo 07 done

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let reverse_string = "";
+    for(i=string.length-1;i>=0;i--){
+        console.log(string[i])
+        reverse_string +=  string[i];
+    }
+    return reverse_string;
 };
-
+console.log(reverseString("hello"))
 // Do not edit below this line
 module.exports = reverseString;
