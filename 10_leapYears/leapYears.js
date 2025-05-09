3b8c0c417fd2b505ba85741cc6e8eebb645425e7
Rino <82651074+Rino9514@users.noreply.github.com>
2025-05-09 13:52:15 +0200

    exo 10 done

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,6 +1,19 @@
-const leapYears = function() {
-
+const leapYears = function(years) {
+    if(typeof years!== "number"){return "ERROR"}
+    if(!Number.isInteger(years)){return "ERROR"}
+    if((years % 4) === 0){
+        if ((years % 100) === 0){
+            if ((years % 400) === 0){
+                return true;
+            }
+            return false;
+        }
+        return true;
+    }
+    else{
+        return false;
+    }
 };
-
+console.log(leapYears(1650));
 // Do not edit below this line
 module.exports = leapYears;
