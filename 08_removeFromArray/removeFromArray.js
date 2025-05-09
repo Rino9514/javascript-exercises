be93acbf6c73d7cfabc62794397c6bbd33d09dcb
Rino <82651074+Rino9514@users.noreply.github.com>
2025-05-09 12:32:03 +0200

    exo 08 done

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(array_to_remove, ...number_to_remove) {
+    newArray = [];
+    array_to_remove.forEach((element) => {
+        if (!number_to_remove.includes(element)) {
+            newArray.push(element)
+        }
+    });
+    return newArray;
 };
-
 // Do not edit below this line
 module.exports = removeFromArray;
