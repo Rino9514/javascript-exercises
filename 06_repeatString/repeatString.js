cf796a8db54f73166f8191b45f530dd0a419afe4
Rino <82651074+Rino9514@users.noreply.github.com>
2025-05-09 12:02:09 +0200

    exo 6 done

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,22 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let return_string = "";
+    if (num === 0) {
+        return "";
+    }
+    else if(num > 0) {
+        if ((typeof string === "string") && (string.length> 0)){
+            for(i=0;i<num;i++){
+                return_string = return_string + string;
+            }
+            return return_string;
+        }
+        return ""
+    }
+    else{
+        return "ERROR"
+    }
 };
 
+console.log(repeatString("zbeub",1));
 // Do not edit below this line
 module.exports = repeatString;
