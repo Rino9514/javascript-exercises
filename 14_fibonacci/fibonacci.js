543499e090935be19e846071633df46ab0e45e40
Rino <82651074+Rino9514@users.noreply.github.com>
2025-05-25 17:21:53 +0200

    fibo done

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+    let converted_index = Number(index);
+    console.log(converted_index)
+    if (typeof converted_index !== "number" || !isFinite(converted_index) || converted_index < 0){
+        return "OOPS";
+    }
+    let array_fibo = [0,1];
+    for(let ii=1; ii<=converted_index;ii++) {
+        array_fibo.push(array_fibo[ii]+array_fibo[ii-1])
+    }
+    return array_fibo[converted_index];
 };
 
 // Do not edit below this line
