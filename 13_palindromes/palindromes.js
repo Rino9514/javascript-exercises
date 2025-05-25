1f9124cff5e03a708666503af51659b7f24d25e0
Rino <82651074+Rino9514@users.noreply.github.com>
2025-05-25 16:44:09 +0200

    palindrome done

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let string_without_poncutation = string.replace(/[.,\/#!$%\^&\*;:{}= \-_`~()'"?!¿¡]/g, "").toLowerCase();
+    for(let ii=0; ii<=Math.floor(string_without_poncutation.length/2); ii++){
+        if (string_without_poncutation[ii] !== string_without_poncutation[string_without_poncutation.length - ii - 1]){
+            return false;
+        } 
+    }
+    return true;
 };
 
 // Do not edit below this line
