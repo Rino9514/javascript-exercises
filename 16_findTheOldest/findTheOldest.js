e23718be2f4ff1b551cd56362c8067890fc540e6
Rino <82651074+Rino9514@users.noreply.github.com>
2025-05-25 18:36:58 +0200

    find_the_oldest done

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,30 @@
-const findTheOldest = function() {
+    const people = [
+      {
+        name: "Carly",
+        yearOfBirth: 1942,
+        yearOfDeath: 1970,
+      },
+      {
+        name: "Ray",
+        yearOfBirth: 1962,
+        yearOfDeath: 2011,
+      },
+      {
+        name: "Jane",
+        yearOfBirth: 1912,
+        yearOfDeath: 1941,
+      },
+    ]
 
+const findTheOldest = function(array) {
+    return array.reduce((oldest, current) => {
+        const oldestAge = isFinite(oldest.yearOfDeath) ? oldest.yearOfDeath - oldest.yearOfBirth : 2025 - oldest.yearOfBirth;
+        if (isFinite(current.yearOfDeath)){
+            return oldestAge < (current.yearOfDeath - current.yearOfBirth) ? current: oldest;
+        } else {
+            return oldestAge < (2025 - current.yearOfDeath) ? current : oldest;
+        }
+    })
 };
-
 // Do not edit below this line
 module.exports = findTheOldest;
